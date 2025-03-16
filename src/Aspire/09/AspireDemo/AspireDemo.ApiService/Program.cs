0b48bfca32b13dda0a932fb9a1c06efa69249d97
marqdouj <16510416+marqdouj@users.noreply.github.com>
2025-03-16 04:04:21 -0600

    added Scalar OpenAPI, added custom exception handler in ApiService

--- src/Aspire/09/AspireDemo/AspireDemo.ApiService/Program.cs
+++ src/Aspire/09/AspireDemo/AspireDemo.ApiService/Program.cs
@@ -1,5 +1,8 @@
 using AspireDemo.ApiService.Endpoints;
 using AspireDemo.ApiService.Services;
+using Marqdouj.CLRCommon;
+using Microsoft.AspNetCore.Diagnostics;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,13 +20,90 @@ builder.ConfigureEmailService();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseExceptionHandler();
+app.UseExceptionHandler(exApp => exApp.Run(async context =>
+{
+    if (context.Response.HasStarted)
+        return;
+
+    var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+    var exMsg = exception?.ToMessage();
+
+
+    if (exception != null)
+    {
+        ILogger<IEmailService>? logger = null;
+        exMsg = $"{exception.Source}: {exMsg}";
+
+        try
+        {
+            try
+            {
+                var factory = exApp.ApplicationServices.GetService<ILoggerFactory>();
+                logger = factory?.CreateLogger<IEmailService>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to create logger: {ex}");
+            }
+
+            using var scope = exApp.ApplicationServices.CreateScope();
+            var emailService = scope.ServiceProvider.GetService<IEmailService>();
+            if (emailService != null)
+                await emailService.SendErrorEmail(exception);
+            else
+            {
+                logger?.LogError(exception, "ExceptionHandler - Failed to get email service.");
+                Console.WriteLine("ExceptionHandler - Failed to get email service.");
+            }
+        }
+        catch (Exception)
+        {
+            logger?.LogError(exception, "ExceptionHandler - SendErrorEmail failed.");
+            Console.WriteLine("ExceptionHandler - SendErrorEmail failed.");
+        }
+    }
+
+    var pds = context.RequestServices.GetService<IProblemDetailsService>();
+    var ok = false;
+
+    if (pds != null)
+    {
+        if (string.IsNullOrWhiteSpace(exMsg))
+        {
+            ok = await pds.TryWriteAsync(new() { HttpContext = context });
+        }
+        else
+        {
+            ok = await pds.TryWriteAsync(new ProblemDetailsContext
+            {
+                HttpContext = context,
+                ProblemDetails =
+                {
+                    Title = exMsg,
+                }
+            });
+        }
+    }
+
+    if (ok is false)
+    {
+        // Fallback behavior
+        await context.Response.WriteAsync($"Fallback: An error has occurred. {exMsg}");
+    }
+}));
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.MapScalarApiReference(options =>
+    {
+        options.DefaultFonts = false; // Disable default fonts to avoid download unnecessary fonts
+        options.Servers = []; //Required in Aspire
+    });
 }
 
+app.MapDevelopment(app.Environment.IsDevelopment());
 app.MapNewsletterApi();
 app.MapWeatherApi();
 app.MapDefaultEndpoints();
